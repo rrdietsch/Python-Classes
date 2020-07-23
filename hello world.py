9a41e1b7f9fc5228b95ef4ebd732228602519fcc
rrdietsch <rrdietsch@gmail.com>
2020-07-22 23:11:02 -0400

    Initial Commit

---
+++ hello world.py
@@ -0,0 +1,24 @@
+import os
+
+os.system('cls')
+'''
+first_name = "Ricardo"
+
+#print(first_name)
+
+first_name = "michelle"
+
+print(first_name)
+'''
+#dictionary
+nombres = ["juan", "Luis", "Ricardo"]
+
+#tuple
+nombres = ("juan", "Luis", "Ricardo")
+
+#dictionaries
+nombre_edad = {
+    "ricardo": "36",
+    "Luis": "34",
+    "Juan": "36",
+    }
---
+++ jamposx.py
@@ -0,0 +1,6 @@
+import os
+
+os.system('cls')
+
+
+print("hello world")
