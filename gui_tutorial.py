b1ee37ad072402c0917ef9ba353298a1a1e8a3ec
Ricardo Ramos <rrdietsch@gmail.com>
2020-08-11 11:31:53 -0400

    test

--- gui_tutorial.py
+++ gui_tutorial.py
@@ -4,5 +4,8 @@ root = tk.Tk()
 label = tk.Label(root, text="my first window in chromeos")
 label.pack()
 
+mybutton = tk.Button(root, text="button", command=print("this is a test"))
+mybutton.pack()
+
 
 root.mainloop()
